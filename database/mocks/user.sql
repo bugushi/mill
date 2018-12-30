482e5296b8086c3b1e37ea009c14a64f9699ac9c
布谷 <yedra@qq.com>
2018-12-30 23:49:03 +0800

    更新readme

--- database/mocks/user.sql
+++ database/mocks/user.sql
@@ -4,9 +4,9 @@ DELETE FROM `user`;
 
 INSERT INTO `user` (`id`, `nick`, `avatar`, `token`, `followings`, `followers`, `created_at`, `updated_at`)
 VALUES
-	(10000,'堺雅人','https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1353675638.54.webp','jieyaren',0,0,'2018-02-24 19:52:28','2018-02-24 19:52:28'),
-	(10001,'新垣结衣','https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1747.webp','xinyuanjieyi',0,0,'2018-02-24 19:52:36','2018-02-24 19:52:36'),
-	(10002,'生濑胜久','https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p56518.webp','shenglaishengjiu',0,0,'2018-02-24 19:52:43','2018-02-24 19:52:43');
+	(10000,'堺雅人','https://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1353675638.54.webp','jieyaren',0,0,'2018-12-30 19:52:28','2018-12-30 19:52:28'),
+	(10001,'新垣结衣','https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1747.webp','xinyuanjieyi',0,0,'2018-12-30 19:52:36','2018-12-30 19:52:36'),
+	(10002,'生濑胜久','https://img1.doubanio.com/view/celebrity/s_ratio_celebrity/public/p56518.webp','shenglaishengjiu',0,0,'2018-12-30 19:52:43','2018-12-30 19:52:43');
 
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
